c317a8d2d15c01e32674e2b423cdd0aa773771a8
GithubCoder2021 <91566024+GithubCoder2021@users.noreply.github.com>
2023-01-11 10:58:40 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,9 @@
 var number = new Decimal(10);
 var cost = new Decimal(10);
 var numPerSec = new Decimal(0);
+
+setInterval(function() {
+  number = Decimal.add(number, numPerSec)
+	
+  document.getElementById("number").innerText = number;
+}, 1000);
